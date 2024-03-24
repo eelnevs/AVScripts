cfe63c6cd8c46e4c3b02b3f7470c20c6965efbcc
eelnevs <84779478+eelnevs@users.noreply.github.com>
2024-03-24 15:36:06 -0700

    Add Hymn num converter lua

---
+++ Hymn-num-converter.lua
@@ -0,0 +1,215 @@
+obs           = obslua
+source_name   = ""
+Eng_number    = 0
+Sp_number     = 0
+Eng_only      = false
+my_settings   = nil
+hymn_table    = {[1]=1,[5]=2,[7]=3,[8]=4,[10]=5,[11]=6,[12]=7,[13]=8,[15]=9,[16]=10,[17]=11,[18]=12,[20]=13,[21]=14,[22]=15,[23]=16,[24]=17,[26]=18,[28]=19,[29]=20,[30]=21,[32]=22,[34]=23,[36]=24,[39]=25,[40]=26,[41]=27,[43]=28,[48]=29,[50]=30,[52]=31,[56]=33,[57]=34,[61]=35,[62]=36,[65]=37,[66]=38,[67]=39,[70]=40,[73]=41,[77]=42,[78]=43,[79]=44,[81]=47,[82]=48,[84]=49,[86]=50,[101]=51,[105]=52,[108]=53,[112]=54,[116]=55,[117]=56,[118]=57,[123]=58,[124]=59,[125]=60,[127]=66,[128]=67,[132]=68,[133]=69,[137]=70,[139]=71,[142]=72,[146]=73,[148]=74,[151]=75,[152]=76,[154]=77,[162]=78,[163]=79,[170]=80,[171]=81,[172]=82,[175]=83,[180]=84,[181]=85,[188]=86,[189]=87,[190]=88,[195]=89,[197]=90,[202]=91,[203]=93,[206]=94,[208]=95,[220]=96,[221]=97,[222]=98,[223]=99,[224]=100,[226]=101,[227]=102,[233]=103,[241]=110,[242]=112,[243]=113,[244]=114,[248]=115,[250]=116,[251]=117,[255]=119,[265]=120,[266]=121,[267]=127,[268]=129,[271]=130,[272]=131,[273]=132,[277]=133,[278]=134,[279]=135,[284]=137,[286]=138,[288]=139,[296]=141,[298]=142,[299]=143,[300]=144,[302]=146,[303]=147,[305]=148,[306]=149,[308]=151,[309]=152,[310]=153,[313]=154,[322]=155,[324]=156,[325]=157,[328]=159,[333]=160,[340]=161,[348]=165,[350]=167,[373]=168,[382]=170,[389]=171,[390]=172,[395]=173,[396]=174,[398]=175,[399]=176,[403]=177,[405]=178,[412]=179,[426]=180,[430]=181,[431]=183,[433]=184,[436]=185,[437]=186,[438]=187,[439]=188,[441]=189,[445]=190,[446]=191,[449]=193,[463]=194,[472]=195,[473]=196,[474]=197,[475]=198,[481]=199,[482]=200,[483]=201,[487]=202,[488]=203,[490]=204,[491]=205,[493]=207,[495]=209,[496]=210,[497]=211,[498]=212,[499]=213,[500]=214,[501]=215,[502]=216,[503]=217,[505]=218,[507]=219,[508]=220,[509]=224,[510]=232,[513]=235,[514]=236,[522]=237,[535]=238,[536]=239,[537]=240,[538]=241,[539]=242,[540]=243,[541]=253,[542]=254,[546]=255,[548]=256,[549]=257,[551]=258,[554]=259,[556]=260,[561]=262,[563]=263,[564]=264,[569]=267,[583]=268,[589]=269,[591]=270,[593]=271,[594]=272,[595]=273,[600]=283,[602]=284,[608]=287,[609]=288,[610]=289,[611]=290,[612]=291,[618]=293,[622]=294,[626]=295,[630]=296,[631]=297,[639]=298,[640]=299,[666]=303,[717]=304,[733]=305,[734]=306,[735]=307,[736]=308,[737]=309,[738]=310,[739]=311,[740]=312,[741]=313,[742]=314,[743]=315,[744]=316,[745]=318,[746]=319,[747]=320,[748]=321,[749]=322,[750]=323,[764]=326,[769]=327,[770]=328,[771]=329,[775]=330,[779]=331,[780]=332,[781]=333,[782]=334,[783]=335,[784]=336,[786]=337,[798]=338,[799]=339,[801]=340,[802]=341,[806]=342,[811]=343,[812]=344,[813]=345,[814]=346,[815]=347,[819]=348,[820]=349,[821]=350,[824]=351,[831]=353,[832]=354,[837]=356,[839]=357,[840]=358,[841]=359,[845]=360,[846]=361,[847]=362,[848]=363,[851]=364,[852]=365,[853]=366,[854]=367,[863]=390,[864]=391,[866]=392,[867]=393,[871]=394,[877]=395,[880]=396,[882]=397,[885]=398,[886]=399,[889]=400,[890]=401,[893]=402,[894]=403,[904]=405,[908]=406,[909]=407,[910]=408,[911]=409,[912]=410,[913]=411,[914]=412,[917]=413,[921]=414,[922]=415,[923]=416,[925]=417,[930]=418,[936]=423,[937]=424,[938]=425,[941]=426,[942]=427,[943]=428,[944]=429,[945]=430,[947]=431,[948]=433,[949]=434,[954]=435,[956]=436,[958]=438,[960]=439,[962]=440,[965]=441,[966]=442,[967]=443,[969]=444,[970]=445,[971]=450,[972]=451,[973]=452,[975]=454,[976]=455,[978]=456,[980]=457,[981]=458,[986]=460,[991]=461,[994]=462,[998]=463,[999]=464,[1002]=465,[1003]=466,[1004]=467,[1005]=468,[1006]=469,[1007]=470,[1008]=471,[1009]=472,[1010]=473,[1017]=474,[1019]=475,[1024]=476,[1025]=274,[1040]=477,[1043]=478,[1045]=479,[1048]=481,[1050]=482,[1051]=483,[1052]=484,[1058]=485,[1059]=486,[1065]=487,[1066]=488,[1068]=489,[1069]=490,[1074]=166,[1080]=491,[1081]=32,[1083]=45,[1086]=46,[1095]=61,[1096]=62,[1097]=63,[1098]=64,[1101]=65,[1103]=92,[1104]=109,[1106]=104,[1107]=105,[1108]=106,[1110]=107,[1112]=108,[1113]=124,[1115]=118,[1116]=125,[1117]=126,[1119]=122,[1121]=123,[1122]=136,[1125]=150,[1127]=162,[1131]=145,[1133]=169,[1134]=182,[1141]=206,[1142]=208,[1143]=225,[1145]=226,[1146]=227,[1148]=228,[1149]=229,[1150]=230,[1151]=231,[1152]=275,[1153]=276,[1154]=277,[1159]=278,[1162]=265,[1163]=266,[1164]=245,[1167]=246,[1168]=247,[1170]=248,[1171]=279,[1173]=280,[1174]=281,[1175]=282,[1178]=249,[1179]=250,[1180]=251,[1181]=252,[1187]=244,[1191]=222,[1193]=223,[1195]=285,[1196]=286,[1198]=292,[1205]=301,[1206]=302,[1212]=324,[1214]=317,[1216]=325,[1220]=352,[1221]=369,[1222]=370,[1223]=371,[1224]=372,[1226]=373,[1229]=374,[1231]=375,[1232]=376,[1234]=377,[1235]=378,[1237]=379,[1243]=355,[1246]=380,[1248]=381,[1251]=382,[1252]=383,[1254]=384,[1255]=385,[1260]=386,[1265]=387,[1273]=388,[1275]=389,[1287]=404,[1293]=419,[1295]=420,[1299]=432,[1302]=446,[1311]=447,[1315]=448,[1319]=449,[1325]=459,[1327]=492,[1331]=493,[1333]=494,[1335]=495,[1337]=496,[1339]=497,[1341]=498,[1345]=499,[1349]=421,[1350]=164,[1351]=422,[1352]=261,[1354]=437,[1355]=140,[1357]=163,[1359]=192,[1360]=128}
+sp_hymn_table = {}
+ovi = obs.obs_video_info();
+
+-- Function to find scene item
+function findSceneItem(sceneName, itemName, groupName)
+	local source = obs.obs_get_source_by_name(sceneName)
+	local item = nil
+	if source then
+		local scene = obs.obs_scene_from_source(source)
+		if scene then
+			item = obs.obs_scene_find_source(scene, itemName)
+			if not item and groupName then
+				local group = obs.obs_scene_find_source(scene, groupName)
+				print("found")
+				if group then 
+					scene = obs.obs_sceneitem_group_get_scene(group)
+					if scene then
+						print("found scene")
+						item = obs.obs_scene_find_source(scene, itemName)
+						if item then
+							print("found item")
+						end
+					end
+				end
+			end
+		end
+		obs.obs_source_release(source)
+	end
+	return item
+end
+
+function currentSceneName()
+	local source = obs.obs_frontend_get_current_scene()
+	local name = obs.obs_source_get_name(source)
+	obs.obs_source_release(source)
+	return name
+end
+
+function centerText(center)
+	local text_sceneItem = findSceneItem(currentSceneName(), source_name, "Don't Click  --->")
+	local transform = obs.obs_transform_info()
+	obs.obs_sceneitem_get_info(text_sceneItem, transform)
+	local textSource = obs.obs_get_source_by_name(source_name)
+	local textWidth = obs.obs_source_get_width(textSource)
+	if center then
+		transform.pos.x = (ovi.base_width - textWidth)/2
+	else
+		transform.pos.x = 50
+	end
+	obs.obs_sceneitem_set_info(text_sceneItem, transform)
+	obs.obs_source_release(textSource)
+end
+
+-- Functions to set the text
+function set_sp_num()
+	if hymn_table[Eng_number] == nil then
+		Sp_number = 0
+	else
+		Sp_number = hymn_table[Eng_number]
+	end
+end
+
+function set_eng_num()
+	if sp_hymn_table[Sp_number] == nil then
+		Eng_number = 0
+	else
+		Eng_number = sp_hymn_table[Sp_number]
+	end
+end
+
+function set_hymn_text(lang_entered)
+	if lang_entered == "Sp" then
+		set_eng_num()
+	else
+		set_sp_num()
+	end
+	--print(Eng_number)
+	--print(Sp_number)
+	local text = ""
+	if Eng_number ~= 0 or Sp_number ~= 0 then
+		local Eng_text = string.format("%d", Eng_number)
+		local Sp_text = string.format("%d", Sp_number)
+		if Sp_number == 0 then
+			Sp_text = "-"
+			--text = string.format("Hymn E%d S-", Eng_number)
+		elseif Eng_number == 0 then
+			Eng_text = "-"
+		end
+		if Eng_only then
+			text = string.format("Hymn %s", Eng_text)
+		else
+			text = string.format("Hymn E%s S%s", Eng_text, Sp_text)
+		end
+	end
+	
+	local source = obs.obs_get_source_by_name(source_name)
+	if source ~= nil then
+		local settings = obs.obs_data_create()
+		obs.obs_data_set_string(settings, "text", text)
+		obs.obs_source_update(source, settings)
+		obs.obs_data_release(settings)
+		obs.obs_source_release(source)
+	end
+	return true
+end
+
+function reset_button_clicked(props, p)
+	local source = obs.obs_get_source_by_name(source_name)
+	if source ~= nil then
+		Eng_number = 0
+		Sp_number = 0
+		set_hymn_text("Eng")
+		obs.obs_data_set_int(my_settings, "Sp", Sp_number)
+		obs.obs_data_set_int(my_settings, "Eng", Eng_number)
+	end
+	return true
+end
+
+function toggle_es_clicked(props, p)
+	centerText(false)
+	Eng_only = not Eng_only
+	set_hymn_text("Eng")
+end
+
+function center_button_clicked(props, p)
+	centerText(true)
+end
+
+function left_align_button_clicked(props, p)
+	centerText(false)
+end
+
+function callback(props, p, settings)
+	centerText(false)
+end 
+----------------------------------------------------------
+
+-- A function named script_properties defines the properties that the user
+-- can change for the entire script module itself
+function script_properties()
+	local props = obs.obs_properties_create()
+	local Eng_num = obs.obs_properties_add_int(props, "Eng", "English Number", 0, 1360, 1)
+	local toggle = obs.obs_properties_add_button(props, "toggle_ES", "Toggle E/S", toggle_es_clicked)
+	local reset = obs.obs_properties_add_button(props, "reset_button", "Clear", reset_button_clicked)
+	local center_btn = obs.obs_properties_add_button(props, "center_button", "Center Text", center_button_clicked)
+	local left_align_btn = obs.obs_properties_add_button(props, "left_align_button", "Align Left", left_align_button_clicked)
+	local Sp_num = obs.obs_properties_add_int(props, "Sp", "Spanish Number", 0, 500, 1)
+	
+	obs.obs_property_set_modified_callback(Eng_num, callback)
+	obs.obs_property_set_modified_callback(Sp_num, callback)
+
+	local p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+	local sources = obs.obs_enum_sources()
+	if sources ~= nil then
+		for _, source in ipairs(sources) do
+			source_id = obs.obs_source_get_unversioned_id(source)
+			if source_id == "text_gdiplus" or source_id == "text_ft2_source" then
+				source_name = obs.obs_source_get_name(source)
+				obs.obs_property_list_add_string(p, source_name, source_name)
+			end
+		end
+	end
+	obs.source_list_release(sources)
+	
+	return props
+end
+
+-- A function named script_description returns the description shown to the user
+function script_description()
+	--return "Sets a text source to the entered English/Spanish Hymn number with corresponding Spanish/English Hymn number."
+end
+
+-- A function named script_update will be called when settings are changed
+function script_update(settings)
+	if obs.obs_data_get_int(settings, "Sp") == Sp_number then
+		Eng_number = obs.obs_data_get_int(settings, "Eng")
+		source_name = obs.obs_data_get_string(settings, "source")
+		set_hymn_text("Eng")
+	elseif obs.obs_data_get_int(settings, "Eng") == Eng_number then 
+		Sp_number = obs.obs_data_get_int(settings, "Sp")
+		source_name = obs.obs_data_get_string(settings, "source")
+		set_hymn_text("Sp")
+	end
+	my_settings = settings
+	obs.obs_data_set_int(settings, "Sp", Sp_number)
+	obs.obs_data_set_int(settings, "Eng", Eng_number)
+end
+
+-- A function named script_defaults will be called to set the default settings
+function script_defaults(settings)
+	obs.obs_data_set_default_int(settings, "Eng", 0)
+	obs.obs_data_set_default_int(settings, "Sp", 0)
+end
+
+function script_unload(settings)
+	obs.obs_data_set_int(settings, "Eng", 0)
+	Eng_number = 0
+	set_hymn_text("Eng")
+end
+
+function script_load(settings)
+	obs.obs_data_set_int(settings, "Eng", 0)
+	obs.obs_data_set_int(settings, "Sp", 0)
+	for k,v in pairs(hymn_table) do
+		sp_hymn_table[v] = k
+	end
+	obs.obs_get_video_info(ovi);
+end
